6ab98ee1c2342ed856384543baed993799322220
Mehmet Özer <115498182+ozermehmett@users.noreply.github.com>
2023-12-12 16:53:02 +0300

    fix tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -16,14 +16,14 @@ def send_voice(update, context):
     text = update.message.text
     context.bot.send_chat_action(chat_id=chat_id, action=ChatAction.UPLOAD_AUDIO)
 
-    voice_file_path = create_folder(str(chat_id))
-
-    voice_file_path = tts(text, voice_file_path)
+    voice_file_path = tts(text, str(chat_id)
 
     context.bot.send_voice(chat_id=update.message.chat_id, voice=InputFile(open(voice_file_path, 'rb')))
 
 
 def start(update, context):
+    chat_id = update.message.chat_id
+    create_folder(str(chat_id))
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text="""This bot converts the text you write in English into an audio recording and sends it to you.""")
 
